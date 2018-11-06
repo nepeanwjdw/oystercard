2e6e91b4c15b951858b235042851c17d47b874e7
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2018-11-06 16:02:46 +0000

    chater 6: have added unit test for maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,19 @@ require 'oystercard'
 
 describe Oystercard do
   it 'should have a balance' do
-    # card = Oystercard.new
     expect(subject.balance).to eq 0
+  end
+  describe '#top_up' do
+    it 'should be to recieve top up from user' do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+    end
+
+    it 'should have a maximum balance of £90' do
+      subject.top_up(90)
+      message = 'The maximum balance is £90'
+      expect { subject.top_up(1) }.to raise_error(message)
+    end
+
   end
 end

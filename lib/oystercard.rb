d45463ca965570ddc7937fbe92bb4b745fa71e1a
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-11-07 15:12:22 +0000

    Challenge 10 - charging the oystercard on touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,10 @@
 class Oystercard
 
-MAXIMUM_BALANCE = 90
-MINIMUM_BALANCE = 1
-attr_reader :balance
+  MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
+  MINIMUM_CHARGE = 1
+
+  attr_reader :balance
 
   def initialize
     @balance = 0
@@ -15,10 +17,6 @@ attr_reader :balance
     @balance += amount
   end
 
-  def deduct(amount)
-    @balance -= amount
-  end
-
   def in_journey?
     @in_transit
   end
@@ -29,6 +27,13 @@ attr_reader :balance
   end
 
   def touch_out
+    deduct(MINIMUM_CHARGE)
     @in_transit = false
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,20 +13,25 @@ describe Oystercard do
         expect(subject.balance).to eq Oystercard::MINIMUM_BALANCE
       end
     end
-    describe '#deduct' do
-      it 'should deduct an amount from the balance' do
-        subject.deduct(1)
-        expect(subject.balance).to eq (Oystercard::MINIMUM_BALANCE-1)
+
+    describe 'a group of tests that also require touch_in' do
+      before do
+        subject.touch_in
       end
-    end
-    it 'when touched out it changes in_transit to false' do
-      subject.touch_in
-      subject.touch_out
-      expect(subject).not_to be_in_journey
-    end
-    it 'when touched in changes in_transit to true' do
-      subject.touch_in
-      expect(subject).to be_in_journey
+      describe '#touch_out' do
+        it 'when touched out it changes in_transit to false' do
+          subject.touch_out
+          expect(subject).not_to be_in_journey
+        end
+        it 'when touched out it deducts the minimum charge' do
+          expect{ subject.touch_out }.to change{ subject.balance }.by(-Oystercard::MINIMUM_CHARGE)
+        end
+      end
+      describe '#touch_in' do
+        it 'when touched in changes in_transit to true' do
+          expect(subject).to be_in_journey
+        end
+      end
     end
   end
 
@@ -40,9 +45,10 @@ describe Oystercard do
     expect(subject).not_to be_in_journey
   end
 
-  it 'when below the minimum balance does not let you touch in' do
-    message = 'Insufficient balance to touch in'
-    expect{ subject.touch_in }.to raise_error message
+  describe '#touch_in' do
+    it 'when below the minimum balance does not let you touch in' do
+      message = 'Insufficient balance to touch in'
+      expect{ subject.touch_in }.to raise_error message
+    end
   end
-
 end

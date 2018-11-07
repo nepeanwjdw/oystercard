922bd40e523e49b4f533999ea12a56b0b0a0fc09
Will Wayman <will.jdw@gmail.com>
2018-11-07 14:17:52 +0000

    grouped tests that required topup and added a before block

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,25 +4,26 @@ describe Oystercard do
   it 'should have a balance' do
     expect(subject.balance).to eq 0
   end
-  describe '#top_up' do
-    it 'should be to recieve top up from user' do
-    subject.top_up(10)
-    expect(subject.balance).to eq 10
-    end
-
-    it 'should have a maximum balance of £90' do
-      subject.top_up(Oystercard::MAXIMUM_BALANCE)
-      message = "The maximum balance is £#{Oystercard::MAXIMUM_BALANCE}"
-      expect { subject.top_up(1) }.to raise_error(message)
-    end
-
-  end
-  describe '#deduct' do
-    it 'should deduct an amount from the balance' do
+  describe 'a group of tests that require a top_up' do
+    before do
       subject.top_up(50)
-      subject.deduct(10)
-      expect(subject.balance).to eq 40
     end
+    describe '#top_up' do
+      it 'should be to recieve top up from user' do
+        expect(subject.balance).to eq 50
+      end
+    end
+    describe '#deduct' do
+      it 'should deduct an amount from the balance' do
+        subject.deduct(10)
+        expect(subject.balance).to eq 40
+      end
+    end
+  end
+  it 'should have a maximum balance of £90' do
+    subject.top_up(Oystercard::MAXIMUM_BALANCE)
+    message = "The maximum balance is £#{Oystercard::MAXIMUM_BALANCE}"
+    expect { subject.top_up(1) }.to raise_error(message)
   end
 
   it 'is initially not in a journey' do
